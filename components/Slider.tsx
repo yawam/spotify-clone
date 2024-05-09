5aa1243ae1fbd472d26794166917ec9c91ee0e48
Papa Yaw Agyeman-Manu <agy20002@byui.edu>
2024-05-09 15:25:55 -0600

    not sure where this is going but we'll see

---
+++ components/Slider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as RadixSlider from "@radix-ui/react-slider";
+
+interface SliderProps {
+  value?: number;
+  onChange?: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+  const handleChange = (newValue: number[]) => {
+    onChange?.(newValue[0]);
+  };
+  return <div>SLider !</div>;
+};
+
+export default Slider;
